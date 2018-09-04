4b57eb6be11c32f3e2bf618e16d407744cb46ba2
Jason <xucong.jason@gmail.com>
2018-09-03 23:14:46 -0400

    for review

--- task.py
+++ task.py
@@ -26,10 +26,26 @@ class Task():
 
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.orig_dist = np.linalg.norm(init_pose[:3] - self.target_pos)
 
-    def get_reward(self):
+    def get_reward(self, is_done):
         """Uses current pose of sim to return reward."""
         reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        dist = np.linalg.norm(self.sim.pose[:3] - self.target_pos)
+        if dist < 0.05 * self.orig_dist:
+            reward += 2.0 * abs(reward)
+        elif dist < 0.1 * self.orig_dist:
+            reward += 1.5 * abs(reward)
+        elif dist < 0.2 * self.orig_dist:
+            reward += 1.0 * abs(reward)
+        elif dist < 0.5 * self.orig_dist:
+            reward += 0.8 * abs(reward)
+        elif dist < 0.8 * self.orig_dist:
+            reward += 0.5 * abs(reward)
+        elif dist < 0.9 * self.orig_dist:
+            reward += 0.2 * abs(reward)
+        elif is_done:
+            reward -= 1.0 * abs(reward)
         # print(self.sim.pose[:3], sigmoid(reward))
         # reward = 1. - .3 * ((self.sim.pose[0] - self.target_pos[0]) ** 2 + (self.sim.pose[1] - self.target_pos[1]) ** 2 + (self.sim.pose[2] - self.target_pos[2]) ** 2)
         return sigmoid(reward)
@@ -40,7 +56,7 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            reward += self.get_reward(done)
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
